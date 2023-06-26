40bc6752de53c5e968b610099ce43254adaa7121
Mark Nesterenko <marxxnest@gmail.com>
2023-06-26 19:51:22 +0300

    added displaying button actions on the screen

--- app/src/main/java/com/example/calculatorbymarkn/MainActivity.kt
+++ app/src/main/java/com/example/calculatorbymarkn/MainActivity.kt
@@ -4,32 +4,105 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+
+        val zeroButton = findViewById<Button>(R.id.zeroButton)
+        val oneButton = findViewById<Button>(R.id.oneButton)
+        val twoButton = findViewById<Button>(R.id.twoButton)
+        val threeButton = findViewById<Button>(R.id.threeButton)
+        val fourButton = findViewById<Button>(R.id.fourButton)
+        val fiveButton = findViewById<Button>(R.id.fiveButton)
+        val sixButton = findViewById<Button>(R.id.sixButton)
+        val sevenButton = findViewById<Button>(R.id.sevenButton)
+        val eightButton = findViewById<Button>(R.id.eightButton)
+        val nineButton = findViewById<Button>(R.id.nineButton)
+        val pointButton = findViewById<Button>(R.id.pointButton)
+        val equalButton = findViewById<Button>(R.id.equalButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+        val minusButton = findViewById<Button>(R.id.minusButton)
+        val divideButton = findViewById<Button>(R.id.divideButton)
+        val multiplyButton = findViewById<Button>(R.id.multiplyButton)
+        val clearButton = findViewById<Button>(R.id.clearButton)
+
+
+        val stringBuilder = StringBuilder()
+
+        zeroButton.setOnClickListener {
+            stringBuilder.append(0)
+            resultTextView.text = stringBuilder
+        }
+        oneButton.setOnClickListener {
+            stringBuilder.append(1)
+            resultTextView.text = stringBuilder
+        }
+        twoButton.setOnClickListener {
+            stringBuilder.append(2)
+            resultTextView.text = stringBuilder
+        }
+        threeButton.setOnClickListener {
+            stringBuilder.append(3)
+            resultTextView.text = stringBuilder
+        }
+        fourButton.setOnClickListener {
+            stringBuilder.append(4)
+            resultTextView.text = stringBuilder
+        }
+        fiveButton.setOnClickListener {
+            stringBuilder.append(5)
+            resultTextView.text = stringBuilder
+        }
+        sixButton.setOnClickListener {
+            stringBuilder.append(6)
+            resultTextView.text = stringBuilder
+        }
+        sevenButton.setOnClickListener {
+            stringBuilder.append(7)
+            resultTextView.text = stringBuilder
+        }
+        eightButton.setOnClickListener {
+            stringBuilder.append(8)
+            resultTextView.text = stringBuilder
+        }
+        nineButton.setOnClickListener {
+            stringBuilder.append(9)
+            resultTextView.text = stringBuilder
+        }
+
+        pointButton.setOnClickListener {
+            stringBuilder.append(".")
+            resultTextView.text = stringBuilder
+        }
+        plusButton.setOnClickListener {
+            stringBuilder.append("+")
+            resultTextView.text = stringBuilder
+        }
+        minusButton.setOnClickListener {
+            stringBuilder.append("-")
+            resultTextView.text = stringBuilder
+        }
+        divideButton.setOnClickListener {
+            stringBuilder.append("/")
+            resultTextView.text = stringBuilder
+        }
+        multiplyButton.setOnClickListener {
+            stringBuilder.append("*")
+            resultTextView.text = stringBuilder
+        }
+        equalButton.setOnClickListener {
+            ///////////////////////////
+        }
+        clearButton.setOnClickListener {
+            stringBuilder.clear()
+            resultTextView.text = "0"
+        }
+
+
     }
-
-    val resultTextView = findViewById<TextView>(R.id.resultTextView)
-
-    val zero_button = findViewById<Button>(R.id.zero_button)
-    val one_button = findViewById<Button>(R.id.one_button)
-    val two_button = findViewById<Button>(R.id.two_button)
-    val three_button = findViewById<Button>(R.id.three_button)
-    val four_button = findViewById<Button>(R.id.four_button)
-    val five_button = findViewById<Button>(R.id.five_button)
-    val six_button = findViewById<Button>(R.id.six_button)
-    val seven_button = findViewById<Button>(R.id.seven_button)
-    val eight_button = findViewById<Button>(R.id.eight_button)
-    val nine_button = findViewById<Button>(R.id.nine_button)
-
-    val point_button = findViewById<Button>(R.id.point_button)
-    val equal_button = findViewById<Button>(R.id.equal_button)
-    val plus_button = findViewById<Button>(R.id.plus_button)
-    val minus_button = findViewById<Button>(R.id.minus_button)
-    val divide_button = findViewById<Button>(R.id.divide_button)
-    val multiply_button = findViewById<Button>(R.id.multiply_button)
-    val clear_button = findViewById<Button>(R.id.clear_button)
-
 }
